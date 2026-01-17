a56b7a78eb5c02da81c02b020d5083d3f505f5ff
Connor J Thompson <ct770a@gmail.com>
2026-01-17 21:14:33 +1100

    removed focus window logic to implement it into the launcher itself

--- src/APIs/Launcher/FocusLauncher.h
+++ src/APIs/Launcher/FocusLauncher.h
@@ -4,37 +4,15 @@
 
 uint32_t launcher_focus_ack = 0;
 
-void BringToFront(HWND hwnd)
-{
-    if (!IsWindow(hwnd))
-        return;
-
-    // Restore if minimized
-    if (IsIconic(hwnd))
-        ShowWindow(hwnd, SW_RESTORE);
-
-    // Allow SetForegroundWindow
-    DWORD fgThread = GetWindowThreadProcessId(GetForegroundWindow(), nullptr);
-    DWORD targetThread = GetWindowThreadProcessId(hwnd, nullptr);
-
-    AttachThreadInput(targetThread, fgThread, TRUE);
-
-    SetForegroundWindow(hwnd);
-    SetFocus(hwnd);
-    SetActiveWindow(hwnd);
-
-    AttachThreadInput(targetThread, fgThread, FALSE);
-}
 void FocusLauncher(bool focus_CGB)
 {
-    // TODO: for now we're going to focus the launcher from here
-    //       this might be a bit more responsive but we'll see i guess
+    // TODO: exception if launcher isn't open??
+    // TOOD: and put this logic elsewhere, like inside he IPC loop
     HWND hwnd = FindWindowA(nullptr, "Server Browser");
     if (!hwnd) {
         MessageBoxA(nullptr, "Failed to find Launcher window to focus.", "Overlay fail", MB_OK | MB_ICONWARNING);
         return;
     }
-    BringToFront(hwnd);
     // write whether to focus the CGB into our ack 
     launcher_focus_ack &= 0xFFFFFFFE;
     launcher_focus_ack |= focus_CGB;
